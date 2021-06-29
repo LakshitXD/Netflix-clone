676812387c3a918b839914727abf6c0c55b0a01f
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2021-06-29 11:14:28 +0530

    basic banner functionality

--- src/Banner.js
+++ src/Banner.js
@@ -1,27 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import axios from './axios'
-import requests from './requests'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
 function Banner() {
-    const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState([]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const request =await axios.get(requests.fetchNetflixOriginals)
-            setMovie(
-                request.data.results
-                [Math.floor(Math.random()*request.data.results.length)]
-            )
-        }
-        fetchData()
-    }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
 
-
-    return (
-        <header>
-
-        </header>
-    )
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+                )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        {/* title */}
+        <h1>{movie.title || movie?.name || movie?.original_name}</h1>
+      </div>
+      <div className="banner__buttons">
+        <button className="banner__button">Play</button>
+        <button className="banner__button">My List</button>
+      </div>
+      <h1 className="banner__description">{movie?.overview}</h1>
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
