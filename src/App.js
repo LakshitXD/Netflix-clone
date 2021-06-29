1dd9e23204e40461189cbe580ab768577fb2a031
lakshit k. singh <66665569+LakshitXD@users.noreply.github.com>
2021-06-29 13:40:43 +0530

    added functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-import Banner from './Banner';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-      {/* Nav */}
+      <Nav />
       <Banner />
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -17,26 +18,31 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
         backgroundImage: `url(
-                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
-                )`,
+          "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
         {/* title */}
-        <h1>{movie.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">{movie.title || movie?.name || movie?.original_name}</h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
       </div>
-      <div className="banner__buttons">
-        <button className="banner__button">Play</button>
-        <button className="banner__button">My List</button>
-      </div>
-      <h1 className="banner__description">{movie?.overview}</h1>
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
